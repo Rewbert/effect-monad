359f6ba4d5c99952f4571288900d7c01fc46a2b4
Rewbert <oamannen@yahoo.com>
2018-03-06 21:46:12 +0100

    Doesn't compile. Useless error message

--- src/Control/CMonad.hs
+++ src/Control/CMonad.hs
@@ -7,20 +7,20 @@ import GHC.Exts ( Constraint )
 
 class CMonad (m :: l -> l -> k -> * -> *) where
   type Identity m :: k
-  type Comp m (a :: l) (f :: k) (g :: k) :: k
+  type Comp m (f :: k) (g :: k) :: k
   type CInv m (f :: k) (g :: k) :: Constraint
-  type CInv m (a :: l) f g = ()
+  type CInv m f g = ()
   return :: a -> m inv inv (Identity m) a
   (>>=) :: CInv m f g
         => m pre int f a 
         -> (a -> m int post g b)
-        -> m pre post (Comp m post f g) b
+        -> m pre post (Comp m f g) b
 
 data Z
 data S n
 
 {-| The counter has no semantic meaning -}
-data Counter i j n a = Counter { forget :: a }
+data Counter pre post n a = Count { forget :: a }
 
 {-| Type-level addition -}
 type family n :+ m 
@@ -28,6 +28,12 @@ type instance n :+ Z     = n
 type instance n :+ (S m) = S (n :+ m)
 
 instance CMonad Counter where
-        type Identity Counter = Z
-        type Comp Counter a f g = f :+ g
-        type CInv Counter a n m = ()
+    type CInv Counter n m = ()
+    type Identity Counter = Z
+    type Comp Counter n m = n :+ m
+
+    -- On the value level we just 'work' with the a
+    return a = Count a
+
+    -- On the value level we just 'work' with the a
+    (Count a) >>= k = Count . forget $ k a
