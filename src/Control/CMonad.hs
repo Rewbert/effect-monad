16420a9e526cca6fe8235a89f38a86e217edac55
Rewbert <oamannen@yahoo.com>
2018-03-06 18:20:04 +0100

    ahahahaha ghc panic

--- src/Control/CMonad.hs
+++ src/Control/CMonad.hs
@@ -1,15 +1,33 @@
-{-# LANGUAGE KindSignatures, TypeFamilies, ConstraintKinds, PolyKinds, MultiParamTypeClasses #-}
+{-# LANGUAGE KindSignatures, TypeOperators, EmptyDataDecls, TypeFamilies, ConstraintKinds, PolyKinds, MultiParamTypeClasses #-}
 
 module Control.CMonad where
 
-import Control.Effect
 import Prelude hiding (Monad(..))
 import GHC.Exts ( Constraint )    
 
 class CMonad (m :: l -> l -> k -> * -> *) where
   type Identity m :: k
-  type Comp m (f :: k) (g :: k) :: k
+  type Comp m (a :: l) (f :: k) (g :: k) :: k
   type CInv m (f :: k) (g :: k) :: Constraint
-  type CInv m f g = ()
+  type CInv m (a :: l) f g = ()
   return :: a -> m inv inv (Identity m) a
-  (>>=) :: CInv m f g => m pre int f a -> (a -> m int post g b) -> m pre post (Comp m f g) b
+  (>>=) :: CInv m f g
+        => m pre int f a 
+        -> (a -> m int post g b)
+        -> m pre post (Comp m post f g) b
+
+data Z
+data S n
+
+{-| The counter has no semantic meaning -}
+data Counter i j n a = Counter { forget :: a }
+
+{-| Type-level addition -}
+type family n :+ m 
+type instance n :+ Z     = n
+type instance n :+ (S m) = S (n :+ m)
+
+instance CMonad Counter where
+        type Identity Counter = Z
+        type Comp Counter a f g = f :+ g
+        type CInv Counter a n m = ()
