ce10f77d1795ee432f7f9230a9f7be33d1ac4c82
BeardedRoar <joeljoel224@hotmail.com>
2018-03-10 17:11:46 +0100

    some comments in CMonad

--- src/Control/CMonad.hs
+++ src/Control/CMonad.hs
@@ -6,7 +6,12 @@ import Prelude hiding (Monad(..))
 import GHC.Exts ( Constraint )
 import qualified Control.Effect as E
 
+{-| Specified our monad, which is a parameterised annotated effect monad.
+    It is built upon the basic monad but with type-level invariants and
+    annotated by a type-level monoid. -}
 class CMonad (m :: l -> l -> k -> * -> *) where
+
+  {-| Effect of a trivially effectful computation -}
   type Identity m :: k
   type EmptyCond m :: l
   type Comp m (f :: k) (g :: k) :: k
@@ -23,13 +28,17 @@ class CMonad (m :: l -> l -> k -> * -> *) where
         -> m pre post (Comp m f g) b
   m >> g = m >>= const g
 
+-- | Specifies subeffecting behaviour
 class Subeffect (m :: l -> l -> k -> * -> *) f g where
-        sub :: m pre post f a -> m pre post g a
+  sub :: m pre post f a -> m pre post g a
 
+-- | Automatic converion of Effect-monads into CMonads
 data Effect m pre post k a where
   Wrap :: (E.Effect m)
        => m (e :: k) a -> Effect m () () (e :: k) a
 
+{- | Run function for the Effect CMonads, results in the respective effect
+     computation -}
 unWrap :: (E.Effect m)
        => Effect m () () e a -> m e a
 unWrap (Wrap m) = m
